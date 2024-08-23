0ef2e60810dfb841b067ee5f88166c6e5655dea3
Якименков Кирилл Александрович <kayakimenk@mts.ru>
2024-08-23 16:52:37 +0300

    [DOP-16697] remove unnessary dto

--- data_rentgen/dto/operation.py
+++ data_rentgen/dto/operation.py
@@ -9,8 +9,6 @@ from enum import Enum
 
 from uuid6 import UUID
 
-from data_rentgen.dto.interaction import InteractionTypeDTO
-
 
 class OperationTypeDTO(str, Enum):
     BATCH = "BATCH"
@@ -41,18 +39,3 @@ class OperationDTO:
     position: int | None = None
     started_at: datetime | None = None
     ended_at: datetime | None = None
-
-
-@dataclass(slots=True)
-class OperationDatasetDTO:
-    operation_id: UUID
-    dataset_id: int
-    interaction_type: InteractionTypeDTO
-
-
-@dataclass(slots=True)
-class OperationNodeDTO:
-    id: UUID
-    name: str
-    status: str
-    type: str
