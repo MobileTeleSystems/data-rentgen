8b607479b156612ef783da2bfae58f2e96816860
Якименков Кирилл Александрович <kayakimenk@mts.ru>
2024-08-23 14:54:53 +0300

    [DOP-16697] Add tests with until for  dataset and operations

--- tests/test_server/test_lineage/test_dataset_lineage.py
+++ tests/test_server/test_lineage/test_dataset_lineage.py
@@ -1,3 +1,4 @@
+from datetime import timedelta
 from http import HTTPStatus
 
 import pytest
@@ -78,10 +79,74 @@ async def test_get_dataset_lineage(
     }
 
 
+dataset_lineage_annotation = tuple[Dataset, list[Operation]]
+
+
 async def test_get_dataset_lineage_with_until(
     test_client: AsyncClient,
     async_session: AsyncSession,
-    lineage: lineage_fixture_annotation,
+    operations_to_dataset_lineage: dataset_lineage_annotation,
 ):
-    _, runs, datasets, operations, _ = lineage
-    # В фикстуре должно создаваться несколько операций с датасетами (3) две связи должны попасть в линадж 1 нет
+    dataset, operations = operations_to_dataset_lineage
+    # Get location for dataset from db
+    query = (
+        select(Location)
+        .join(Dataset, Dataset.location_id == Location.id)
+        .options(selectinload(Location.addresses))
+        .where(Dataset.id == dataset.id)
+    )
+    location = await async_session.scalar(query)
+    since = operations[0].created_at
+    until = since + timedelta(seconds=1)
+    # Create expected results
+    dataset_nodes = [
+        {
+            "kind": "DATASET",
+            "id": dataset.id,
+            "format": dataset.format,
+            "name": dataset.name,
+            "location": {
+                "name": location.name,
+                "type": location.type,
+                "addresses": [{"url": address.url} for address in location.addresses],
+            },
+        },
+    ]
+    operation_nodes = [
+        {
+            "kind": "OPERATION",
+            "id": str(operation.id),
+            "run_id": str(operation.run_id),
+            "name": operation.name,
+            "status": operation.status.value,
+            "type": operation.type.value,
+            "position": operation.position,
+            "description": operation.description,
+            "started_at": operation.started_at.strftime("%Y-%m-%dT%H:%M:%SZ"),
+            "ended_at": operation.ended_at.strftime("%Y-%m-%dT%H:%M:%SZ"),
+        }
+        for operation in operations[:2]
+    ]
+    relations = [
+        {
+            "from": {"id": str(operation.id), "kind": "OPERATION"},
+            "kind": "INTERACTION",
+            "to": {"id": dataset.id, "kind": "DATASET"},
+            "type": "APPEND",
+        }
+        for operation in operations[:2]
+    ]
+
+    response = await test_client.get(
+        "v1/lineage",
+        params={
+            "since": since.isoformat(),
+            "until": until.isoformat(),
+            "point_kind": "DATASET",
+            "point_id": dataset.id,
+            "direction": "TO",
+        },
+    )
+
+    assert response.status_code == HTTPStatus.OK, response.json()
+    assert response.json() == {"nodes": dataset_nodes + operation_nodes, "relations": relations}
--- tests/test_server/test_lineage/test_operation_lineage.py
+++ tests/test_server/test_lineage/test_operation_lineage.py
@@ -75,3 +75,76 @@ async def test_get_operation_lineage(
             },
         ],
     }
+
+
+operation_lineage_annotation = tuple[Operation, list[Dataset]]
+
+
+async def get_operation_lineage_with_until(
+    test_client: AsyncClient,
+    async_session: AsyncSession,
+    operation_to_datasets_lineage: operation_lineage_annotation,
+):
+    operation, datasets = operation_to_datasets_lineage
+    # Get location for dataset from db
+    query = (
+        select(Location)
+        .join(Dataset, Dataset.location_id == Location.id)
+        .options(selectinload(Location.addresses))
+        .where(Dataset.id == datasets[0].id)
+    )
+    location = await async_session.scalar(query)
+    since = operation.created_at
+    until = since + timedelta(seconds=1)
+    # Create expected results
+    dataset_nodes = [
+        {
+            "kind": "DATASET",
+            "id": dataset.id,
+            "format": dataset.format,
+            "name": dataset.name,
+            "location": {
+                "name": location.name,
+                "type": location.type,
+                "addresses": [{"url": address.url} for address in location.addresses],
+            },
+        }
+        for dataset in datasets[:2]
+    ]
+    operation_nodes = [
+        {
+            "kind": "OPERATION",
+            "id": str(operation.id),
+            "run_id": str(operation.run_id),
+            "name": operation.name,
+            "status": operation.status.value,
+            "type": operation.type.value,
+            "position": operation.position,
+            "description": operation.description,
+            "started_at": operation.started_at.strftime("%Y-%m-%dT%H:%M:%SZ"),
+            "ended_at": operation.ended_at.strftime("%Y-%m-%dT%H:%M:%SZ"),
+        },
+    ]
+    relations = [
+        {
+            "from": {"id": str(operation.id), "kind": "OPERATION"},
+            "kind": "INTERACTION",
+            "to": {"id": dataset.id, "kind": "DATASET"},
+            "type": "APPEND",
+        }
+        for dataset in datasets[:2]
+    ]
+
+    response = await test_client.get(
+        "v1/lineage",
+        params={
+            "since": since.isoformat(),
+            "until": until.isoformat(),
+            "point_kind": "OPERATION",
+            "point_id": operation.id,
+            "direction": "FROM",
+        },
+    )
+
+    assert response.status_code == HTTPStatus.OK, response.json()
+    assert response.json() == {"nodes": dataset_nodes + operation_nodes, "relations": relations}
