88097b7388b7846fff1b47f524b487ddab75453c
Якименков Кирилл Александрович <kayakimenk@mts.ru>
2024-09-17 18:09:30 +0300

    [DOP-19281] Add image publication, union server and consumer into one image

--- docker/consumer.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-exec python -m data_rentgen.consumer "$@"
--- docker/server.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-python -m data_rentgen.db.migrations upgrade head
-python -m data_rentgen.db.scripts.create_partitions
-
-exec python -m data_rentgen.server --host 0.0.0.0 --port 8000 "$@"
