6b471d93a6d146c34857c1415fb8154f21297371
Якименков Кирилл Александрович <kayakimenk@mts.ru>
2024-11-12 11:26:43 +0300

    [DOP-21294] refactor fixtures for operations

--- tests/test_server/fixtures/factories/run.py
+++ tests/test_server/fixtures/factories/run.py
@@ -5,7 +5,6 @@ from typing import AsyncContextManager, Callable
 
 import pytest
 import pytest_asyncio
-from sqlalchemy import delete
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from data_rentgen.db.models import Address, Job, Run, RunStartReason, RunStatus, User
