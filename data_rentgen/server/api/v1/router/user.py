fde2f8b24166f1c0508d95a785fb999739f158e8
Якименков Кирилл Александрович <kayakimenk@mts.ru>
2024-12-19 14:48:44 +0300

    [DOP-21576] Small fixes

--- data_rentgen/server/api/v1/router/user.py
+++ data_rentgen/server/api/v1/router/user.py
@@ -6,11 +6,6 @@ from fastapi import APIRouter, Depends
 
 from data_rentgen.db.models.user import User
 from data_rentgen.server.errors.registration import get_error_responses
-from data_rentgen.server.errors.schemas.invalid_request import InvalidRequestSchema
-from data_rentgen.server.errors.schemas.not_authorized import (
-    NotAuthorizedRedirectSchema,
-    NotAuthorizedSchema,
-)
 from data_rentgen.server.schemas.v1.user import UserResponseV1
 from data_rentgen.server.services import get_user
 
@@ -20,13 +15,13 @@ logger = getLogger(__name__)
 router = APIRouter(
     prefix="/users",
     tags=["User"],
-    responses=get_error_responses(include={NotAuthorizedSchema, InvalidRequestSchema, NotAuthorizedRedirectSchema}),
+    responses=get_error_responses(),
 )
 
 
 @router.get("/me")
 async def check_auth(
     current_user: User = Depends(get_user()),
-):
+) -> UserResponseV1:
     logger.info("User check: %s", current_user.name)
     return UserResponseV1.model_validate(current_user)
