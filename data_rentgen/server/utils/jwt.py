25229a7dd14cf556405d1efc957e425838262fa7
Мартынов Максим Сергеевич <msmarty5@mts.ru>
2024-12-24 18:47:50 +0300

    [DOP-23149] Replace python-jose with pyjwt

--- data_rentgen/server/utils/jwt.py
+++ data_rentgen/server/utils/jwt.py
@@ -1,28 +1,25 @@
 # SPDX-FileCopyrightText: 2024 MTS PJSC
 # SPDX-License-Identifier: Apache-2.0
-from jose import ExpiredSignatureError, JWTError, jwt
+import jwt
 
 from data_rentgen.exceptions.auth import AuthorizationError
 
 
 def sign_jwt(payload: dict, secret_key: str, security_algorithm: str) -> str:
     return jwt.encode(
-        payload,
-        secret_key,
+        payload=payload,
+        key=secret_key,
         algorithm=security_algorithm,
     )
 
 
 def decode_jwt(token: str, secret_key: str, security_algorithm: str) -> dict:
     try:
-        result = jwt.decode(
-            token,
-            secret_key,
-            algorithms=[security_algorithm],
-        )
-        if "exp" not in result:
-            raise ExpiredSignatureError("Missing expiration time in token")
+        claims = jwt.decode(jwt=token, key=secret_key, algorithms=[security_algorithm])
 
-        return result
-    except JWTError as e:
+        if "exp" not in claims:
+            raise jwt.exceptions.ExpiredSignatureError("Missing expiration time in token")
+
+        return claims
+    except jwt.exceptions.InvalidTokenError as e:
         raise AuthorizationError("Invalid token") from e
