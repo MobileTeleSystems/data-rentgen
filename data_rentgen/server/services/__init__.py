869ef61586c3300b6c7eccf3881b9ede68a173ca
Мартынов Максим Сергеевич <msmarty5@mts.ru>
2025-01-28 14:48:04 +0300

    [DOP-22129] Update license headers

--- data_rentgen/server/services/__init__.py
+++ data_rentgen/server/services/__init__.py
@@ -1,4 +1,4 @@
-# SPDX-FileCopyrightText: 2024 MTS PJSC
+# SPDX-FileCopyrightText: 2024-2025 MTS PJSC
 # SPDX-License-Identifier: Apache-2.0
 from data_rentgen.server.services.get_user import get_user
 from data_rentgen.server.services.lineage import LineageService
