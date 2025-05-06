d07b6ae2fbb7534689f271d4be8409b93800ccff
Якименков Кирилл Александрович <kayakimenk@mts.ru>
2025-05-06 17:30:30 +0300

    [DOP-25645] insert license

--- data_rentgen/dto/sql_query.py
+++ data_rentgen/dto/sql_query.py
@@ -1,3 +1,5 @@
+# SPDX-FileCopyrightText: 2024-2025 MTS PJSC
+# SPDX-License-Identifier: Apache-2.0
 from __future__ import annotations
 
 from dataclasses import dataclass, field
@@ -7,6 +9,10 @@ from dataclasses import dataclass, field
 class SQLQueryDTO:
     query: str
     id: int | None = field(default=None, compare=False)
+
+    @property
+    def unique_key(self) -> tuple:
+        return (self.query,)
 
     def merge(self, new: SQLQueryDTO) -> SQLQueryDTO:
         if new.id is None:
