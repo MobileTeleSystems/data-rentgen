7d56427294b327573d15cc25e6feac9e00e5da35
Мартынов Максим Сергеевич <msmarty5@mts.ru>
2025-06-11 14:51:56 +0300

    [DOP-26449] Capture Flink's jobId

--- tests/test_consumer/test_openlineage/test_run_event_flink.py
+++ tests/test_consumer/test_openlineage/test_run_event_flink.py
@@ -1,6 +1,7 @@
 from datetime import datetime, timezone
 
 import pytest
+from packaging.version import Version
 from pydantic import TypeAdapter
 from uuid6 import UUID
 
@@ -28,6 +29,11 @@ from data_rentgen.consumer.openlineage.run_event import (
     OpenLineageRunEvent,
     OpenLineageRunEventType,
 )
+from data_rentgen.consumer.openlineage.run_facets import (
+    OpenLineageFlinkJobDetailsRunFacet,
+    OpenLineageProcessingEngineRunFacet,
+    OpenLineageRunFacets,
+)
 
 RunEventAdapter = TypeAdapter(OpenLineageRunEvent)
 
@@ -42,19 +48,32 @@ RunEventAdapter = TypeAdapter(OpenLineageRunEvent)
 def test_run_event_flink_job_start(processing_type: str, expected_job_type: OpenLineageJobProcessingType):
     json = {
         "eventTime": "2025-04-22T08:37:53.938068Z",
-        "producer": "https://github.com/OpenLineage/OpenLineage/tree/1.32.0/integration/flink",
+        "producer": "https://github.com/OpenLineage/OpenLineage/tree/1.34.0/integration/flink",
         "schemaURL": "https://openlineage.io/spec/2-0-2/OpenLineage.json#/$defs/RunEvent",
         "eventType": "START",
         "run": {
             "runId": "01965ca5-85bc-789d-93a1-9e4e48bd37b3",
-            "facets": {},
+            "facets": {
+                "flink_job": {
+                    "_producer": "https://github.com/OpenLineage/OpenLineage/tree/1.34.0/integration/flink",
+                    "_schemaURL": "https://openlineage.io/spec/facets/2-0-3/JobTypeJobFacet.json#/$defs/JobTypeJobFacet",
+                    "jobId": "b825f524-49d6-4dd8-bffd-3e5742c528d0",
+                },
+                "processing_engine": {
+                    "_producer": "https://github.com/OpenLineage/OpenLineage/tree/1.34.0/integration/flink",
+                    "_schemaURL": "https://openlineage.io/spec/facets/1-0-1/ProcessingEngineRunFacet.json#/$defs/ProcessingEngineRunFacet",
+                    "version": "1.19.0",
+                    "name": "flink",
+                    "openlineageAdapterVersion": "1.34.0-SNAPSHOT",
+                },
+            },
         },
         "job": {
             "namespace": "http://localhost:18081",
             "name": "flink_job",
             "facets": {
                 "jobType": {
-                    "_producer": "https://github.com/OpenLineage/OpenLineage/tree/1.32.0/integration/flink",
+                    "_producer": "https://github.com/OpenLineage/OpenLineage/tree/1.34.0/integration/flink",
                     "_schemaURL": "https://openlineage.io/spec/facets/2-0-3/JobTypeJobFacet.json#/$defs/JobTypeJobFacet",
                     "processingType": processing_type,
                     "integration": "FLINK",
@@ -68,12 +87,12 @@ def test_run_event_flink_job_start(processing_type: str, expected_job_type: Open
                 "name": "input_topic",
                 "facets": {
                     "documentation": {
-                        "_producer": "https://github.com/OpenLineage/OpenLineage/tree/1.32.0/integration/flink",
+                        "_producer": "https://github.com/OpenLineage/OpenLineage/tree/1.34.0/integration/flink",
                         "_schemaURL": "https://openlineage.io/spec/facets/1-0-1/DocumentationDatasetFacet.json#/$defs/DocumentationDatasetFacet",
                         "description": "My Complex Table",
                     },
                     "schema": {
-                        "_producer": "https://github.com/OpenLineage/OpenLineage/tree/1.32.0/integration/flink",
+                        "_producer": "https://github.com/OpenLineage/OpenLineage/tree/1.34.0/integration/flink",
                         "_schemaURL": "https://openlineage.io/spec/facets/1-1-1/SchemaDatasetFacet.json#/$defs/SchemaDatasetFacet",
                         "fields": [
                             {
@@ -104,7 +123,7 @@ def test_run_event_flink_job_start(processing_type: str, expected_job_type: Open
                         ],
                     },
                     "symlinks": {
-                        "_producer": "https://github.com/OpenLineage/OpenLineage/tree/1.32.0/integration/flink",
+                        "_producer": "https://github.com/OpenLineage/OpenLineage/tree/1.34.0/integration/flink",
                         "_schemaURL": "https://openlineage.io/spec/facets/1-0-1/SymlinksDatasetFacet.json#/$defs/SymlinksDatasetFacet",
                         "identifiers": [
                             {
@@ -123,12 +142,12 @@ def test_run_event_flink_job_start(processing_type: str, expected_job_type: Open
                 "name": "output_topic",
                 "facets": {
                     "documentation": {
-                        "_producer": "https://github.com/OpenLineage/OpenLineage/tree/1.32.0/integration/flink",
+                        "_producer": "https://github.com/OpenLineage/OpenLineage/tree/1.34.0/integration/flink",
                         "_schemaURL": "https://openlineage.io/spec/facets/1-0-1/DocumentationDatasetFacet.json#/$defs/DocumentationDatasetFacet",
                         "description": "",
                     },
                     "schema": {
-                        "_producer": "https://github.com/OpenLineage/OpenLineage/tree/1.32.0/integration/flink",
+                        "_producer": "https://github.com/OpenLineage/OpenLineage/tree/1.34.0/integration/flink",
                         "_schemaURL": "https://openlineage.io/spec/facets/1-1-1/SchemaDatasetFacet.json#/$defs/SchemaDatasetFacet",
                         "fields": [
                             {
@@ -164,7 +183,7 @@ def test_run_event_flink_job_start(processing_type: str, expected_job_type: Open
                         ],
                     },
                     "symlinks": {
-                        "_producer": "https://github.com/OpenLineage/OpenLineage/tree/1.32.0/integration/flink",
+                        "_producer": "https://github.com/OpenLineage/OpenLineage/tree/1.34.0/integration/flink",
                         "_schemaURL": "https://openlineage.io/spec/facets/1-0-1/SymlinksDatasetFacet.json#/$defs/SymlinksDatasetFacet",
                         "identifiers": [
                             {
@@ -194,7 +213,280 @@ def test_run_event_flink_job_start(processing_type: str, expected_job_type: Open
             ),
         ),
         run=OpenLineageRun(
-            runId=UUID("01965CA5-85BC-789d-93a1-9e4e48bd37b3"),
+            runId=UUID("01965ca5-85bc-789d-93a1-9e4e48bd37b3"),
+            facets=OpenLineageRunFacets(
+                # https://github.com/OpenLineage/OpenLineage/pull/3744
+                flink_job=OpenLineageFlinkJobDetailsRunFacet(
+                    jobId="b825f524-49d6-4dd8-bffd-3e5742c528d0",
+                ),
+                # https://github.com/OpenLineage/OpenLineage/pull/3726
+                processing_engine=OpenLineageProcessingEngineRunFacet(
+                    version=Version("1.19.0"),
+                    name="flink",
+                    openlineageAdapterVersion=Version("1.34.0"),
+                ),
+            ),
+        ),
+        inputs=[
+            OpenLineageInputDataset(
+                namespace="kafka://kafka-host1:9092",
+                name="input_topic",
+                facets=OpenLineageDatasetFacets(
+                    documentation=OpenLineageDocumentationDatasetFacet(description="My Complex Table"),
+                    schema=OpenLineageSchemaDatasetFacet(
+                        fields=[
+                            OpenLineageSchemaField(
+                                name="price",
+                                type="DECIMAL(38, 18)",
+                                description="",
+                            ),
+                            OpenLineageSchemaField(
+                                name="currency",
+                                type="STRING",
+                                description="",
+                            ),
+                            OpenLineageSchemaField(
+                                name="log_date",
+                                type="DATE",
+                                description="",
+                            ),
+                            OpenLineageSchemaField(
+                                name="log_time",
+                                type="TIME(0)",
+                                description="",
+                            ),
+                            OpenLineageSchemaField(
+                                name="log_ts",
+                                type="TIMESTAMP(3)",
+                                description="",
+                            ),
+                        ],
+                    ),
+                    # https://github.com/OpenLineage/OpenLineage/pull/3657
+                    symlinks=OpenLineageSymlinksDatasetFacet(
+                        identifiers=[
+                            OpenLineageSymlinkIdentifier(
+                                namespace="kafka://kafka-host1:9092",
+                                name="default_catalog.default_database.kafka_input",
+                                type=OpenLineageSymlinkType.TABLE,
+                            ),
+                        ],
+                    ),
+                ),
+            ),
+        ],
+        outputs=[
+            OpenLineageOutputDataset(
+                namespace="kafka://kafka-host2:9092",
+                name="output_topic",
+                facets=OpenLineageDatasetFacets(
+                    documentation=OpenLineageDocumentationDatasetFacet(description=""),
+                    schema=OpenLineageSchemaDatasetFacet(
+                        fields=[
+                            OpenLineageSchemaField(
+                                name="ts_interval",
+                                type="STRING",
+                                description="",
+                            ),
+                            OpenLineageSchemaField(
+                                name="max_log_date",
+                                type="STRING",
+                                description="",
+                            ),
+                            OpenLineageSchemaField(
+                                name="max_log_time",
+                                type="STRING",
+                                description="",
+                            ),
+                            OpenLineageSchemaField(
+                                name="max_ts",
+                                type="STRING",
+                                description="",
+                            ),
+                            OpenLineageSchemaField(
+                                name="counter",
+                                type="BIGINT",
+                                description="",
+                            ),
+                            OpenLineageSchemaField(
+                                name="max_price",
+                                type="DECIMAL(38, 18)",
+                                description="",
+                            ),
+                        ],
+                    ),
+                    # https://github.com/OpenLineage/OpenLineage/pull/3657
+                    symlinks=OpenLineageSymlinksDatasetFacet(
+                        identifiers=[
+                            OpenLineageSymlinkIdentifier(
+                                namespace="kafka://kafka-host2:9092",
+                                name="default_catalog.default_database.kafka_output",
+                                type=OpenLineageSymlinkType.TABLE,
+                            ),
+                        ],
+                    ),
+                ),
+            ),
+        ],
+    )
+
+
+def test_run_event_flink_job_start_before_openlineage_1_34():
+    json = {
+        "eventTime": "2025-04-22T08:37:53.938068Z",
+        "producer": "https://github.com/OpenLineage/OpenLineage/tree/ 1.33.0/integration/flink",
+        "schemaURL": "https://openlineage.io/spec/2-0-2/OpenLineage.json#/$defs/RunEvent",
+        "eventType": "START",
+        "run": {
+            "runId": "01965ca5-85bc-789d-93a1-9e4e48bd37b3",
+            "facets": {},
+        },
+        "job": {
+            "namespace": "http://localhost:18081",
+            "name": "flink_job",
+            "facets": {
+                "jobType": {
+                    "_producer": "https://github.com/OpenLineage/OpenLineage/tree/ 1.33.0/integration/flink",
+                    "_schemaURL": "https://openlineage.io/spec/facets/2-0-3/JobTypeJobFacet.json#/$defs/JobTypeJobFacet",
+                    "processingType": "BATCH",
+                    "integration": "FLINK",
+                    "jobType": "JOB",
+                },
+            },
+        },
+        "inputs": [
+            {
+                "namespace": "kafka://kafka-host1:9092",
+                "name": "input_topic",
+                "facets": {
+                    "documentation": {
+                        "_producer": "https://github.com/OpenLineage/OpenLineage/tree/ 1.33.0/integration/flink",
+                        "_schemaURL": "https://openlineage.io/spec/facets/1-0-1/DocumentationDatasetFacet.json#/$defs/DocumentationDatasetFacet",
+                        "description": "My Complex Table",
+                    },
+                    "schema": {
+                        "_producer": "https://github.com/OpenLineage/OpenLineage/tree/ 1.33.0/integration/flink",
+                        "_schemaURL": "https://openlineage.io/spec/facets/1-1-1/SchemaDatasetFacet.json#/$defs/SchemaDatasetFacet",
+                        "fields": [
+                            {
+                                "name": "price",
+                                "type": "DECIMAL(38, 18)",
+                                "description": "",
+                            },
+                            {
+                                "name": "currency",
+                                "type": "STRING",
+                                "description": "",
+                            },
+                            {
+                                "name": "log_date",
+                                "type": "DATE",
+                                "description": "",
+                            },
+                            {
+                                "name": "log_time",
+                                "type": "TIME(0)",
+                                "description": "",
+                            },
+                            {
+                                "name": "log_ts",
+                                "type": "TIMESTAMP(3)",
+                                "description": "",
+                            },
+                        ],
+                    },
+                    "symlinks": {
+                        "_producer": "https://github.com/OpenLineage/OpenLineage/tree/ 1.33.0/integration/flink",
+                        "_schemaURL": "https://openlineage.io/spec/facets/1-0-1/SymlinksDatasetFacet.json#/$defs/SymlinksDatasetFacet",
+                        "identifiers": [
+                            {
+                                "namespace": "kafka://kafka-host1:9092",
+                                "name": "default_catalog.default_database.kafka_input",
+                                "type": "TABLE",
+                            },
+                        ],
+                    },
+                },
+            },
+        ],
+        "outputs": [
+            {
+                "namespace": "kafka://kafka-host2:9092",
+                "name": "output_topic",
+                "facets": {
+                    "documentation": {
+                        "_producer": "https://github.com/OpenLineage/OpenLineage/tree/ 1.33.0/integration/flink",
+                        "_schemaURL": "https://openlineage.io/spec/facets/1-0-1/DocumentationDatasetFacet.json#/$defs/DocumentationDatasetFacet",
+                        "description": "",
+                    },
+                    "schema": {
+                        "_producer": "https://github.com/OpenLineage/OpenLineage/tree/ 1.33.0/integration/flink",
+                        "_schemaURL": "https://openlineage.io/spec/facets/1-1-1/SchemaDatasetFacet.json#/$defs/SchemaDatasetFacet",
+                        "fields": [
+                            {
+                                "name": "ts_interval",
+                                "type": "STRING",
+                                "description": "",
+                            },
+                            {
+                                "name": "max_log_date",
+                                "type": "STRING",
+                                "description": "",
+                            },
+                            {
+                                "name": "max_log_time",
+                                "type": "STRING",
+                                "description": "",
+                            },
+                            {
+                                "name": "max_ts",
+                                "type": "STRING",
+                                "description": "",
+                            },
+                            {
+                                "name": "counter",
+                                "type": "BIGINT",
+                                "description": "",
+                            },
+                            {
+                                "name": "max_price",
+                                "type": "DECIMAL(38, 18)",
+                                "description": "",
+                            },
+                        ],
+                    },
+                    "symlinks": {
+                        "_producer": "https://github.com/OpenLineage/OpenLineage/tree/ 1.33.0/integration/flink",
+                        "_schemaURL": "https://openlineage.io/spec/facets/1-0-1/SymlinksDatasetFacet.json#/$defs/SymlinksDatasetFacet",
+                        "identifiers": [
+                            {
+                                "namespace": "kafka://kafka-host2:9092",
+                                "name": "default_catalog.default_database.kafka_output",
+                                "type": "TABLE",
+                            },
+                        ],
+                    },
+                },
+            },
+        ],
+    }
+
+    assert RunEventAdapter.validate_python(json) == OpenLineageRunEvent(
+        eventTime=datetime(2025, 4, 22, 8, 37, 53, 938068, tzinfo=timezone.utc),
+        eventType=OpenLineageRunEventType.START,
+        job=OpenLineageJob(
+            namespace="http://localhost:18081",
+            name="flink_job",
+            facets=OpenLineageJobFacets(
+                jobType=OpenLineageJobTypeJobFacet(
+                    processingType=OpenLineageJobProcessingType.BATCH,
+                    integration="FLINK",
+                    jobType="JOB",
+                ),
+            ),
+        ),
+        run=OpenLineageRun(
+            runId=UUID("01965ca5-85bc-789d-93a1-9e4e48bd37b3"),
         ),
         inputs=[
             OpenLineageInputDataset(
