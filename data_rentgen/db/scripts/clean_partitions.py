e4acb954fa4f6218af7ea40f0e00d665544b047b
Якименков Кирилл Александрович <kayakimenk@mts.ru>
2025-07-01 17:41:59 +0300

    [DOP-26065] add ci step; refactor script

--- data_rentgen/db/scripts/clean_partitions.py
+++ data_rentgen/db/scripts/clean_partitions.py
@@ -21,6 +21,8 @@ from sqlalchemy import text
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from data_rentgen.db.factory import create_session_factory
+from data_rentgen.db.models import Input, Operation, Output, Run
+from data_rentgen.db.models.column_lineage import ColumnLineage
 from data_rentgen.db.settings import DatabaseSettings
 from data_rentgen.logging.settings import LoggingSettings
 from data_rentgen.logging.setup_logging import setup_logging
@@ -29,11 +31,18 @@ logger = logging.getLogger(__name__)
 
 PARTITION_GRANULARITY_PATERN = re.compile(r"(\w+)_y(\d{4})(?:_m(\d{1,2})(?:_d(\d{1,2}))?)?")
 
+PARTITIONED_TABLES = (
+    Run.__tablename__,
+    Operation.__tablename__,
+    Input.__tablename__,
+    Output.__tablename__,
+    ColumnLineage.__tablename__,
+)
 
 granularity_output_format = {
-    "year": "y%Y",
-    "month": "y%Y_m%m",
-    "day": "y%Y_m%m_d%d",
+    "year": "_y%Y",
+    "month": "_y%Y_m%m",
+    "day": "_y%Y_m%m_d%d",
 }
 
 
@@ -46,7 +55,7 @@ class Command(str, Enum):
 
 def get_parser() -> ArgumentParser:
     parser = ArgumentParser(
-        usage="python3 -m data_rentgen.db.scripts.clean_partitions command truncate --keep-after 2025-01-01",
+        usage="python3 -m data_rentgen.db.scripts.clean_partitions truncate --keep-after 2025-01-01",
         description="Truncate or detach partitions, before provided date.",
     )
     parser.add_argument(
@@ -55,7 +64,7 @@ def get_parser() -> ArgumentParser:
         choices=[item.value for item in Command],
         default=Command.DRY_RUN.value,
         nargs="?",
-        help="Operation mode.",
+        help="Operation mode",
     )
     parser.add_argument(
         "--keep-after",
@@ -68,17 +77,21 @@ def get_parser() -> ArgumentParser:
 
 
 @dataclass()
-class TabelPartitions:
-    partitions: list[date] = field(default_factory=list)
+class TablePartition:
+    name: str
+    date: date
     granularity: Literal["year", "month", "day"] = field(default="year")
 
+    @property
+    def qualified_name(self):
+        return self.name + self.date.strftime(granularity_output_format[self.granularity])
 
-async def get_partitioned_tables(session: AsyncSession) -> dict[str, TabelPartitions] | None:
-    tables: dict[str, TabelPartitions] = defaultdict(TabelPartitions)
-    query = text(
-        "select c.relname as table_name from pg_class c where c.relispartition = True and c.relkind = 'r' order by c.relname",  # noqa: E501
-    )
-    result = await session.execute(query)
+
+async def get_partitioned_tables(session: AsyncSession) -> dict[str, list[TablePartition]] | None:
+    tables: dict[str, list[TablePartition]] = defaultdict(list)
+    query = "select c.relname as table_name from pg_class c where c.relispartition = True and c.relkind = 'r' order by c.relname"  # noqa: E501
+    query = query.format(partitioned_tables=PARTITIONED_TABLES)
+    result = await session.execute(text(query))
     table_names = result.all()
     if not table_names:
         logger.info("There is no partitioned tables")
@@ -99,53 +112,52 @@ async def get_partitioned_tables(session: AsyncSession) -> dict[str, TabelPartit
             granularity = "day"
         else:
             day = 1
-        tables[name].partitions.append(date(year, month, day))
-        tables[name].granularity = granularity
+        tables[name].append(TablePartition(name=name, date=date(year, month, day), granularity=granularity))
     return tables
 
 
-def get_partitions(tables: dict[str, TabelPartitions], end_date: date):
-    for table_name, value in tables.items():
-        partitions_to_remove = [partition for partition in value.partitions if partition < end_date]
-        tables[table_name].partitions = partitions_to_remove
-    return tables
+def get_partitions(tables: dict[str, list[TablePartition]], end_date: date) -> dict[str, list[TablePartition]]:
+    tables_to_remove: dict[str, list[TablePartition]] = defaultdict(list)
+    for table_name, partitions in tables.items():
+        tables_to_remove[table_name].extend(partition for partition in partitions if partition.date < end_date)
+    return tables_to_remove
 
 
-def get_query(tables: dict[str, TabelPartitions], query_type: str):
+def get_query(tables: dict[str, list[TablePartition]], query_type: str) -> list[str]:
     queries = []
     query_template = ""
     match query_type:
         case "detach":
-            query_template = "ALTER TABLE {table_name} DETACH PARTITION {table_name}_{partition};"
+            query_template = "ALTER TABLE {table_name} DETACH PARTITION {qualified_name};"
         case "remove":
-            query_template = "DROP TABLE {table_name}_{partition};"
+            query_template = "DROP TABLE {qualified_name};"
         case "truncate":
-            query_template = "TRUNCATE TABLE {table_name}_{partition};"
-    for table_name, value in tables.items():
+            query_template = "TRUNCATE TABLE {qualified_name};"
+    for table_name, partitions in tables.items():
+        if not partitions:
+            logger.info("No partitions of %s to %s", table_name, query_type)
+            continue
         queries_per_table = [
             query_template.format(
                 table_name=table_name,
-                partition=partition.strftime(granularity_output_format[value.granularity]),
+                qualified_name=partition.qualified_name,
             )
-            for partition in value.partitions
+            for partition in partitions
         ]
         queries.extend(queries_per_table)
     return queries
 
 
-def show_removing_partitions(tables: dict[str, TabelPartitions]):
-    for table_name, value in tables.items():
+def show_removing_partitions(tables: dict[str, list[TablePartition]]):
+    for table_name, partitions in tables.items():
         logger.info("Partitions to remove from table: %s", table_name)
         partitions_names = " ".join(
-            [
-                f"{table_name}_{partition.strftime(granularity_output_format[value.granularity])}"
-                for partition in value.partitions
-            ],
+            [partition.qualified_name for partition in partitions],
         )
         logger.info(partitions_names)
 
 
-async def detach_partitions(tables: dict[str, TabelPartitions], session: AsyncSession):
+async def detach_partitions(tables: dict[str, list[TablePartition]], session: AsyncSession):
     partitions_to_detach = get_query(tables, "detach")
     for query in partitions_to_detach:
         logger.debug("Detach partitions with query: %s", query)
@@ -153,7 +165,7 @@ async def detach_partitions(tables: dict[str, TabelPartitions], session: AsyncSe
         await session.commit()
 
 
-async def remove_data(tables: dict[str, TabelPartitions], session: AsyncSession):
+async def remove_data(tables: dict[str, list[TablePartition]], session: AsyncSession):
     partitions_to_remove = get_query(tables, "remove")
     for query in partitions_to_remove:
         logger.debug("Remove table with query: %s", query)
@@ -161,7 +173,7 @@ async def remove_data(tables: dict[str, TabelPartitions], session: AsyncSession)
         await session.commit()
 
 
-async def truncate_data(tables: dict[str, TabelPartitions], session: AsyncSession):
+async def truncate_data(tables: dict[str, list[TablePartition]], session: AsyncSession):
     partitions_to_truncate = get_query(tables, "truncate")
     for query in partitions_to_truncate:
         logger.debug("Truncate partition with query: %s", query)
@@ -178,22 +190,19 @@ async def main(args: list[str]) -> None:
     session_factory = create_session_factory(db_settings)
     async with session_factory() as session:
         tables = await get_partitioned_tables(session)
-        tables_to_remove = get_partitions(
-            tables.copy(),  # type: ignore[union-attr]
-            end_date,
-        )
+        tables_to_remove = get_partitions(tables, end_date)  # type: ignore[arg-type]
         match params.command:
             case "dry_run":
                 show_removing_partitions(tables_to_remove)
             case "detach_partitions":
-                logger.info("detach_partitions")
+                logger.info("Detach partitions starting from: %s", end_date)
                 await detach_partitions(tables_to_remove, session)
             case "remove_data":
-                logger.info("remove_data")
+                logger.info("Remove data from partitions starting from: %s", end_date)
                 await detach_partitions(tables_to_remove, session)
                 await remove_data(tables_to_remove, session)
             case "truncate":
-                logger.info("truncate")
+                logger.info("Truncate partitions starting from: %s", end_date)
                 await truncate_data(tables_to_remove, session)
             case _:
                 logger.error("No such command: %s", params.command)
