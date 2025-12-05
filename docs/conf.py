c9d781cdf9354ec7b9b736a4ad94bbd1bc4b9ab2
Мартынов Максим Сергеевич <msmarty5@mts.ru>
2025-12-05 13:37:34 +0300

    Update team name

--- docs/conf.py
+++ docs/conf.py
@@ -25,7 +25,7 @@ sys.path.insert(0, os.fspath(PROJECT_ROOT_DIR))
 
 project = "Data.Rentgen"
 copyright = "2024-present MTS PJSC"
-author = "DataOps.ETL"
+author = "MWS Data Bridge"
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
