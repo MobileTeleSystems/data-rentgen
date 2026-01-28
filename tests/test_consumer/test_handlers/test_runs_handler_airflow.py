fb76b65c8067b79adce67bb0832539a19dd154b2
Мартынов Максим Сергеевич <msmarty5@mts.ru>
2026-01-28 16:08:24 +0300

    [DOP-31832] Extract Airflow job & run tags

--- tests/test_consumer/test_handlers/test_runs_handler_airflow.py
+++ tests/test_consumer/test_handlers/test_runs_handler_airflow.py
@@ -81,7 +81,8 @@ async def test_runs_handler_airflow(
 
     expected_tag_values = {
         "airflow.version": "2.10.5",
-        "openlineage_adapter.version": "2.2.0",
+        "openlineage_adapter.version": "2.3.0",
+        "environment": "production",
     }
 
     assert len(jobs) == 2
@@ -114,11 +115,15 @@ async def test_runs_handler_airflow(
     assert dag_run.status == RunStatus.SUCCEEDED
     assert dag_run.started_at == datetime(2024, 7, 5, 9, 4, 13, 979349, tzinfo=timezone.utc)
     assert dag_run.ended_at == datetime(2024, 7, 5, 9, 8, 5, 691973, tzinfo=timezone.utc)
-    assert dag_run.started_by_user_id is None
-    assert dag_run.start_reason is None
+    assert dag_run.started_by_user_id is not None
+    assert dag_run.started_by_user_id == user.id
+    assert dag_run.start_reason == RunStartReason.MANUAL
     assert dag_run.end_reason is None
-    assert dag_run.persistent_log_url is None
-    assert dag_run.running_log_url is None
+    assert dag_run.external_id == "manual__2024-07-05T09:04:12.162809+00:00"
+    assert dag_run.attempt is None
+    assert dag_run.persistent_log_url == (
+        "http://airflow-host:8081/dags/mydag/grid?dag_run_id=manual__2024-07-05T09%3A04%3A12.162809%2B00%3A00"
+    )
 
     task_run = runs[1]
     assert task_run.id == UUID("01908223-0782-7fc0-9d69-b1df9dac2c60")
